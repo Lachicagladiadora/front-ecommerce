886662c1eaff39f8f6d9f94d6a1e854d0a660b61
luis <luistenan@gmail.com>
2023-10-23 16:35:56 -0400

    chore: fix lint style

--- src/services/authLogin.ts
+++ src/services/authLogin.ts
@@ -1,28 +1,26 @@
-import { APIURLs, errorWithStatus } from "./settings";
-import { type Session } from './interfaceSession';
+import { APIURLs, type errorWithStatus } from './settings'
+import { type Session } from './interfaceSession'
 
-
-export async function logIn(email: string, password: string): Promise<Session> {
-    const response = await fetch(APIURLs.authLogIn, {
-        method: 'POST',
-        credentials: 'include',
-        headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            email,
-            password
-        })
-    });
-    if (response.ok) {
-        const data = await response.json();
-        return data;
-    } else {
-
-        const error = new Error(`Session error: ${response.status}`) as errorWithStatus;
-        error.status = response.status;
-        error.message = response.statusText;
-        throw error;
-    }
+export async function logIn (email: string, password: string): Promise<Session> {
+  const response = await fetch(APIURLs.authLogIn, {
+    method: 'POST',
+    credentials: 'include',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      email,
+      password
+    })
+  })
+  if (response.ok) {
+    const data = await response.json()
+    return data
+  } else {
+    const error = new Error(`Session error: ${response.status}`) as errorWithStatus
+    error.status = response.status
+    error.message = response.statusText
+    throw error
+  }
 }
--- src/services/authLogout.ts
+++ src/services/authLogout.ts
@@ -1,22 +1,22 @@
-import { APIURLs, errorWithStatus } from "./settings";
-import { type Session } from './interfaceSession';
+import { APIURLs, type errorWithStatus } from './settings'
+import { type Session } from './interfaceSession'
 
-export async function logOut(): Promise<Session> {
-    const response = await fetch(APIURLs.authLogoOut, {
-        method: 'POST',
-        credentials: 'include',
-        headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json',
-        },
-    });
-    if (response.ok) {
-        const data = await response.json();
-        return data;
-    } else {
-        const error = new Error(`Session error: ${response.status}`) as errorWithStatus;
-        error.status = response.status;
-        error.message = response.statusText;
-        throw error;
+export async function logOut (): Promise<Session> {
+  const response = await fetch(APIURLs.authLogoOut, {
+    method: 'POST',
+    credentials: 'include',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
     }
+  })
+  if (response.ok) {
+    const data = await response.json()
+    return data
+  } else {
+    const error = new Error(`Session error: ${response.status}`) as errorWithStatus
+    error.status = response.status
+    error.message = response.statusText
+    throw error
+  }
 }
