648dfcbac28930f4be86f6c05d97f0b9daf779e5
luis <luistenan@gmail.com>
2023-10-23 17:00:06 -0400

    fix: add right return type

--- src/services/removeFromWishList.ts
+++ src/services/removeFromWishList.ts
@@ -1,24 +1,25 @@
-import { APIURLs, errorWithStatus } from "./settings";
+import { APIURLs, type errorWithStatus } from './settings'
+import { type WishList } from './interfaceWishList'
 
-export async function removeFromWishList(wishListId: string, productId: string) {
-    const response = await fetch(APIURLs.removeFromWishList(wishListId), {
-        method: 'POST',
-        credentials: 'include',
-        headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            productId
-        })
-    });
-    if (response.ok) {
-        const data = await response.json();
-        return data;
-    } else {
-        const error = new Error() as errorWithStatus;
-        error.status = response.status;
-        error.message = response.statusText;
-        throw error;
-    }
+export async function removeFromWishList (wishListId: number, productId: number): Promise<WishList> {
+  const response = await fetch(APIURLs.removeFromWishList(wishListId), {
+    method: 'POST',
+    credentials: 'include',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      productId
+    })
+  })
+  if (response.ok) {
+    const data = await response.json()
+    return data
+  } else {
+    const error = new Error() as errorWithStatus
+    error.status = response.status
+    error.message = response.statusText
+    throw error
+  }
 }
