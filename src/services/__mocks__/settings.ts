85a6be7456e9bcf80d266d80458c84a25d7a8b5b
luis <luistenan@gmail.com>
2023-10-25 16:44:23 -0400

    chore: Set jest config to make service testing

---
+++ src/services/__mocks__/settings.ts
@@ -0,0 +1,45 @@
+const baseApiURL = 'apiUrl'
+
+export const APIURLs = {
+
+  // User
+  createUser: `${baseApiURL}/users`,
+
+  // Edit cartList
+  getCartList: (cartListId: number) => `${baseApiURL}/cartList/${cartListId}`,
+  // add and remove
+  addToCartList: (cartListId: number) => `${baseApiURL}/cartList/${cartListId}/add`,
+  removeFromCartList: (cartListId: number) => `${baseApiURL}/cartList/${cartListId}/remove`,
+
+  // Product
+  getProduct: (productId: number) => `${baseApiURL}/products/${productId}`,
+
+  // Product list
+  getProducts: `${baseApiURL}/products`,
+
+  // Auth
+  authLogIn: `${baseApiURL}/auth/login`,
+  authLogoOut: `${baseApiURL}/auth/logout`,
+  authStatus: `${baseApiURL}/auth/status`,
+
+  // Wish list
+  getWishList: (wishListId: number) => `${baseApiURL}/wishList/${wishListId}`,
+  // add and remove
+  addToWishList: (wishListId: number) => `${baseApiURL}/wishList/${wishListId}/add`,
+  removeFromWishList: (wishListId: number) => `${baseApiURL}/wishList/${wishListId}/remove`,
+
+  // Categories
+  // men's clothing
+  getMensClothing: `${baseApiURL}/categories/mens-clothing`,
+  // women's clothing
+  getWomensClothing: `${baseApiURL}/categories/womens-clothing`,
+  // electronics
+  getElectronics: `${baseApiURL}/categories/electronics`,
+  // jewelry
+  getJewelry: `${baseApiURL}/categories/jewelery`
+
+}
+
+export interface errorWithStatus extends Error {
+  status?: number
+}
